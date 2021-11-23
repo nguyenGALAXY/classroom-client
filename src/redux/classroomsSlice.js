8f05a3e80c555219ac763538c2750c16b68524c5
nguyenGALAXY <67467375+nguyenGALAXY@users.noreply.github.com>
2021-11-23 13:26:17 +0700

    remove console

--- src/redux/classroomsSlice.js
+++ src/redux/classroomsSlice.js
@@ -14,7 +14,6 @@ export const createClassroom = createAsyncThunk(
   'classrooms/createClassroom',
   async (classroom) => {
     const response = await axiosClient.post('api/classrooms', classroom)
-    console.log(response.data)
     return response.data
   }
 )
