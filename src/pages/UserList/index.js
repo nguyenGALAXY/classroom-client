58fa6fb3a41a601a1bcb3fae231f2dd660ee9545
nguyenGALAXY <67467375+nguyenGALAXY@users.noreply.github.com>
2021-12-22 12:17:39 +0700

    delete console log

--- src/pages/UserList/index.js
+++ src/pages/UserList/index.js
@@ -137,7 +137,6 @@ const UserList = () => {
       const response = await axiosClient.post(`/api/classrooms/${id}/upload`, {
         data,
       })
-      console.log('data', response.data)
       enqueueSnackbar(response.data.message, { variant: 'success' })
     } catch (error) {
       console.log(error)
